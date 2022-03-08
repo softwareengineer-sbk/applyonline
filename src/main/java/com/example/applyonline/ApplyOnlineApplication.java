f9737ca6ad81fc1c02e6496932a3b54c23df1758
KhalimboevSardorbek <khalimboevsardorbek@gmail.com>
2022-03-09 00:39:10 +0500

    first commit

---
+++ src/main/java/com/example/applyonline/ApplyOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.applyonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplyOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applyonline/ApplyOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.applyonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+
+@SpringBootTest
+class ApplyOnlineApplicationTests {
+
+    @Test
+    void testMain() {
+        assertTrue("Passed", true);
+    }
+}
